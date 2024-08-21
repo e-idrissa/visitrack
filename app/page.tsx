fe7c923acb889daedc8ea18c16dcb2b4cb9753a1
e-idrissa <id.hemedy98@gmail.com>
2024-08-21 15:02:57 +0200

    login responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,18 @@ export default function Home() {
       <div className="col-span-1 flex items-center justify-center">
         <LoginForm />
       </div>
-      <div className="reltive hidden md:block bg-gradient-to-tr from-[#9181fa] to-[#5038ed] min-h-screen p-40">
-        <div className="rounded-2xl bg-white/20 p-8 border border-white font-semibold text-white tracking-wide text-2xl w-96 pb-40">
-          Very good<br />
-          works are<br />
-          waiting for<br />
-          you Login<br />
-          Now!!!
+      <div className="relative hidden md:flex bg-gradient-to-tr from-[#9181fa] to-[#5038ed] min-h-screen p-40 items-center justify-center">
+        <div className="relative rounded-2xl bg-white/20 p-4 lg:p-8 border border-white font-semibold text-white tracking-wide text-lg lg:text-xl xl:text-2xl w-80 xl:pb-40">
+          <span>
+            Very good<br />
+            works are<br />
+            waiting for<br />
+            you Login<br />
+            Now!!!
+          </span>
+          <Image src="/woman.png" alt="photo" width={1000} height={1000} className="absolute hidden xl:block -bottom-[1px] -right-10"/>
+          <Image src="/logo.png" alt="photo" width={40} height={40} className="absolute bottom-12 -left-5"/>
         </div>
-        <Image src="/woman.png" alt="photo" width={500} height={400} className="absolute bottom-44 right-16"/>
-        <Image src="/logo.png" alt="photo" width={40} height={40} className="absolute bottom-52 right-[30rem]"/>
       </div>
     </main>
   );
