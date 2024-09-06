cfd3be3283394f6f07eb2238dc2f33856e920aa8
e-idrissa <id.hemedy98@gmail.com>
2024-09-06 14:58:11 +0200

    og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,22 @@ import { Toaster } from "@/components/ui/sonner";
 export const metadata: Metadata = {
   title: "VisiTrack",
   description: "A Visits Tracking Web App",
+  openGraph: {
+    title: "VisiTrack",
+    description: "A Visits Tracking Web App",
+    url: "https://visitrack.vercel.app", // Replace with your website URL
+    siteName: "VisiTrack",
+    images: [
+      {
+        url: "https://visitrack.vercel.app/public/images/og.png", // Replace with the URL to your OG image
+        width: 842,
+        height: 595,
+        alt: "VisiTrack Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
